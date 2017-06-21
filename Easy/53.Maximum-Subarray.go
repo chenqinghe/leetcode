06d7ec4665ba981b4303cab260cde22ad0c40934
chenqinghe <chenqh721@foxmail.com>
2017-06-22 00:43:06 +0800

    testing 6 failed. to be fixed.

--- Easy/53.Maximum-Subarray.go
+++ Easy/53.Maximum-Subarray.go
@@ -1,7 +1,38 @@
 package leetcode
 
-func MaxSubArray(nums []int) int {
+import "fmt"
 
-	
-	return 1
+//思路：从头尾向中部“夹逼”
+func MaxSubArray(nums []int) int {
+	l := len(nums)
+	if l <= 1 {
+		return nums[0]
+	}
+	head := 0
+	foot := l
+	headsubarrsum := 0
+	footsubarrsum := 0
+	for i := 0; i < l; i++ {
+		if i >= l-i { //遍历完成
+			break
+		}
+		headsubarrsum += nums[i]
+		footsubarrsum += nums[l-1-i]
+		if headsubarrsum <= 0 { //子串和小于零，抛弃
+			head = i + 1
+			headsubarrsum = 0
+		}
+		if footsubarrsum <= 0 { //子串和小于零，抛弃
+			foot = l - i - 1
+			footsubarrsum = 0
+		}
+		fmt.Println(head, foot)
+	}
+
+	nums = nums[head:foot]
+	sum := 0
+	for _, v := range nums {
+		sum += v
+	}
+	return sum
 }
--- Easy/53.Maximum-Subarray_test.go
+++ Easy/53.Maximum-Subarray_test.go
@@ -2,6 +2,44 @@ package leetcode
 
 import "testing"
 
-func TestMaxSubArray(t *testing.T) {
+func TestMaxSubArray1(t *testing.T) {
+	if i := MaxSubArray([]int{1, 2, 3}); i != 6 {
+		t.Fatal("testing failed.", i)
+	}
+	t.Log("testing passed.")
+}
 
+func TestMaxSubArray2(t *testing.T) {
+	if i := MaxSubArray([]int{-1, 2, 3}); i != 5 {
+		t.Fatal("testing failed.", i)
+	}
+	t.Log("testing passed.")
+}
+
+func TestMaxSubArray3(t *testing.T) {
+	if i := MaxSubArray([]int{-2, 1, -3, 4, -1, 2, 1, -5, 4}); i != 6 {
+		t.Fatal("testing failed.", i)
+	}
+	t.Log("testing passed.")
+}
+
+func TestMaxSubArray4(t *testing.T) {
+	if i := MaxSubArray([]int{0}); i != 0 {
+		t.Fatal("testing failed.", i)
+	}
+	t.Log("testing passed.")
+}
+
+func TestMaxSubArray5(t *testing.T) {
+	if i := MaxSubArray([]int{-1, 1}); i != 1 {
+		t.Fatal("testing failed.", i)
+	}
+	t.Log("testing passed.")
+}
+
+func TestMaxSubArray6(t *testing.T) {
+	if i := MaxSubArray([]int{1, -1, 2}); i != 2   {
+		t.Fatal("testing failed.", i)
+	}
+	t.Log("testing passed.")
 }
