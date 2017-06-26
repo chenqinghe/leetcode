54a0f30a547bbae1471d4c98109ef103895d057e
chenqinghe <chenqh721@foxmail.com>
2017-06-27 00:09:54 +0800

    come up a solution

--- Easy/189.Rotate-Array.go
+++ Easy/189.Rotate-Array.go
@@ -6,9 +6,19 @@ func Rotate(nums []int, k int) {
 	l := len(nums)
 	k = k % l
 	fmt.Println(nums)
-	for i := 0; i < l; i++ {
-		tmp := nums[(i+k)%l]
-		nums[(i+k)%l] = nums[i]
-	}
+	reverse(nums, 0, l-1)
+	reverse(nums, 0, k-1)
+	reverse(nums, k, l-1)
 	fmt.Println(nums)
+}
+
+func reverse(nums []int, start int, end int) {
+	for start < end {
+		var tmp int
+		tmp = nums[start]
+		nums[start] = nums[end]
+		nums[end] = tmp
+		start++
+		end--
+	}
 }
